8f0829078be240385a7cb35e616c6a40ea694bf8
Gina González <gini_go@hotmail.com>
2025-05-28 18:15:37 +0200

    add banner-home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
 import { CountdownComponent } from '../countdown/countdown.component';
 import { CardComponent } from '../card/card.component';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent,CountdownComponent,CardComponent],
+  imports: [HeroComponent,CountdownComponent,CardComponent,ButtonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
