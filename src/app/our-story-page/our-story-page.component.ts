01fb9eda0e07801f5026c75932c29ab8bec5f09c
Gina González <gini_go@hotmail.com>
2025-05-30 17:21:24 +0200

    add footer to our story page

--- src/app/our-story-page/our-story-page.component.ts
+++ src/app/our-story-page/our-story-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-our-story-page',
-  imports: [HeroComponent],
+  imports: [HeroComponent,FooterComponent],
   templateUrl: './our-story-page.component.html',
   styleUrl: './our-story-page.component.scss'
 })
