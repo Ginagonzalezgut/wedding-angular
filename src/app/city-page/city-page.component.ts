e1549e3397f798930bff79851d35ee03c13f8115
Gina González <gini_go@hotmail.com>
2025-05-30 17:22:35 +0200

    add footer to city page

--- src/app/city-page/city-page.component.ts
+++ src/app/city-page/city-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-city-page',
-  imports: [HeroComponent],
+  imports: [HeroComponent,FooterComponent],
   templateUrl: './city-page.component.html',
   styleUrl: './city-page.component.scss'
 })
