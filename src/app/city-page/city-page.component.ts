db73957473fd9ab1e8e337c13b7d80f536dfc6e5
Gina González <gini_go@hotmail.com>
2025-06-02 15:43:05 +0200

    add city section

--- src/app/city-page/city-page.component.ts
+++ src/app/city-page/city-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
 import { FooterComponent } from '../footer/footer.component';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-city-page',
-  imports: [HeroComponent,FooterComponent],
+  imports: [HeroComponent,FooterComponent,CardComponent],
   templateUrl: './city-page.component.html',
   styleUrl: './city-page.component.scss'
 })
