c79779bf33908068b3b7d3cef3a8b3ff80a70fe4
Gina González <gini_go@hotmail.com>
2025-06-21 13:26:25 +0200

    add hero to faq page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,19 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CityPageComponent } from './city-page/city-page.component';
 import { OurStoryPageComponent } from './our-story-page/our-story-page.component';
+import { FaqComponent } from './faq/faq.component';
 
 export const routes: Routes = [
-    {path:"",component:HomeComponent,title:"Giselle & Fernando"},
-    {path:"la-ciudad",component:CityPageComponent,title:"La Ciudad"},
-{path:"nuestra-historia",component:OurStoryPageComponent,title:"Nuestra Historia"},
+  { path: '', component: HomeComponent, title: 'Giselle & Fernando' },
+  { path: 'la-ciudad', component: CityPageComponent, title: 'La Ciudad' },
+  {
+    path: 'nuestra-historia',
+    component: OurStoryPageComponent,
+    title: 'Nuestra Historia',
+  },
+  {
+    path: 'faq',
+    component: FaqComponent,
+    title: 'Preguntas Frecuentes',
+  },
 ];
