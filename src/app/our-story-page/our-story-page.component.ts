036f19ee43212b683d6ab10caaacb55d42871fdf
Gina González <gini_go@hotmail.com>
2025-07-07 19:32:19 +0200

    Multiple changes

--- src/app/our-story-page/our-story-page.component.ts
+++ src/app/our-story-page/our-story-page.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
 import { FooterComponent } from '../footer/footer.component';
 import { StoryMomentComponent } from '../story-moment/story-moment.component';
+import { LinkComponent } from '../link/link.component';
 
 @Component({
   selector: 'app-our-story-page',
-  imports: [HeroComponent, FooterComponent, StoryMomentComponent],
+  imports: [
+    HeroComponent,
+    FooterComponent,
+    StoryMomentComponent,
+    LinkComponent,
+  ],
   templateUrl: './our-story-page.component.html',
   styleUrl: './our-story-page.component.scss',
 })
