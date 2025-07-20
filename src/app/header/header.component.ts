4329e3aaf3b06d96d2f92646446a7dd012b34256
Gina González <gini_go@hotmail.com>
2025-07-21 00:21:09 +0200

    Add mobile menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,11 +2,14 @@ import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
 import { LinkComponent } from '../link/link.component';
+import { IconComponent } from '../icon/icon.component';
 
 @Component({
   selector: 'app-header',
-  imports: [NgOptimizedImage, MenuComponent, LinkComponent],
+  imports: [NgOptimizedImage, MenuComponent, LinkComponent, IconComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  mobileMenuOpen = false;
+}
